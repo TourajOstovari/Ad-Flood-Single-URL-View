8b66219745b3771087d9f09e028251b4638f8a8f
Touraj Ostovari <Toraj.ostovari@gmail.com>
2020-12-06 21:09:51 +0330

    Add project files.

---
+++ Ad-Flood/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ad_Flood
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for(int z = 0; z< 2000; z++){
+                    Form2 f = new Form2();
+                    f.Urls = textBox1.Text;
+                    f.Show();
+                }
+            }
+            catch (Exception)
+            {
+                System.Threading.Thread.Sleep(10000);
+                Environment.Exit(0);
+            }
+
+        }
+
+        private void Browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
